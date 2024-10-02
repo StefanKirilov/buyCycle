cdad653b2ae26ff212c52758f4cb76a78f98e08e
Stefan Kirilov <stefank5248@gmail.com>
2024-10-02 10:31:22 +0300

    fourth commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+app.use(cors({
+    origin: 'http://localhost:5173',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(routes);
+
+// mongoose.connect('mongodb://localhost:27017/sushi')
+// .then(() => console.log('DB connected'));
+
+app.listen(5000, () => console.log('Sever is running on port 5000'));
