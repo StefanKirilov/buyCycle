832829250ef2aa6c93c690a61210ffb6c8949c6f
Stefan Kirilov <stefank5248@gmail.com>
2024-10-05 10:11:55 +0300

    new commit

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 
 const userController = require('./controllers/userController');
+const cycleController = require('./controllers/cycleController');
+
+const commentController = require('./controllers/commentController');
 
 router.use('/users', userController);
+router.use('/cycles', cycleController);
+
+router.use('/comments', commentController);
 
 module.exports = router;
