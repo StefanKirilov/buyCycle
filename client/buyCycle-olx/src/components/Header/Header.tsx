caf427cedcb856c3926d779f2ebbd7f4ded49af0
Stefan Kirilov <stefank5248@gmail.com>
2024-10-08 10:49:44 +0300

    commit

---
+++ client/buyCycle-olx/src/components/Header/Header.tsx
@@ -0,0 +1,40 @@
+import styles from './header.module.css';
+
+import { Link, NavLink } from 'react-router-dom';
+
+import logo from '../../assets/logo.svg';
+import chat from '../../assets/chat.svg';
+import favorite from '../../assets/favorite.svg';
+import notifications from '../../assets/notifications.svg';
+import person from '../../assets/person.svg';
+
+export default function Header() {
+
+
+    return(
+        <header className={styles.header}>
+            <nav className={styles.navigation}>
+                <Link to='/' className={styles.logoWrapper}>
+                <h3 className={styles.buy}>BUY</h3>
+                <img className={styles.logo} src={logo} alt='logo'/>
+                </Link>
+                <div className={styles.menu}>
+                <NavLink to="/messages" className={({ isActive }: { isActive: boolean }): string  => (isActive ? styles.active : '') + ' ' + styles.link} >
+                <p>Съобщения</p>
+                <img src={chat} alt="message" />
+                </NavLink>
+                <NavLink to='/likes' className={({ isActive }: { isActive: boolean }): string  => (isActive ? styles.active : '') + ' ' + styles.link}><img src={favorite} alt="heart" /></NavLink>
+                <NavLink to='/notification' className={({ isActive }: { isActive: boolean }): string  => (isActive ? styles.active : '') + ' ' + styles.link}><img src={notifications} alt="notifications" /></NavLink>
+                <NavLink to='/profile' className={({ isActive }: { isActive: boolean }): string  => (isActive ? styles.active : '') + ' ' + styles.link}>
+                <img src={person} alt="person" />
+                <p>Твоят Профил</p>
+                </NavLink>
+                <NavLink to='/create' className={({ isActive }: { isActive: boolean }): string  => (isActive ? styles.active : '') + ' ' + styles.link}>Добави обавя</NavLink>
+                <NavLink to='/logout' className={({ isActive }: { isActive: boolean }): string  => (isActive ? styles.active : '') + ' ' + styles.link}>Изход</NavLink>
+                <NavLink to='/login' className={({ isActive }: { isActive: boolean }): string  => (isActive ? styles.active : '') + ' ' + styles.link}>Вход</NavLink>
+                <NavLink to='/register' className={({ isActive }: { isActive: boolean }): string  => (isActive ? styles.active : '') + ' ' + styles.link}>Регистрация</NavLink>
+                </div>
+            </nav >
+        </header>
+    )
+}
