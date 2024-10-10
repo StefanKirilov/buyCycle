526fccc8197a8fa87970dcc465a1e0a87ab382d0
Stefan Kirilov <stefank5248@gmail.com>
2024-10-10 11:47:58 +0300

    new commit

---
+++ client/buyCycle-olx/src/components/Search/Search.tsx
@@ -0,0 +1,13 @@
+import styles from './search.module.css';
+
+import { Link } from 'react-router-dom';
+
+export default function Search() {
+
+
+    return(
+        <>
+        <h1>Hello</h1>
+        </>
+    )
+}
