fcfd9724d887ae14463f7601bdcecfb37fde5186
Stefan Kirilov <stefank5248@gmail.com>
2024-10-10 11:48:39 +0300

    new commit

---
+++ client/buyCycle-olx/src/components/Home/Home.tsx
@@ -0,0 +1,15 @@
+import styles from './home.module.css';
+
+import Catalog from '../Catalog/Catalog'
+
+import { Link } from 'react-router-dom';
+
+export default function Home() {
+
+
+    return(
+        <>
+        <Catalog />
+        </>
+    )
+}
