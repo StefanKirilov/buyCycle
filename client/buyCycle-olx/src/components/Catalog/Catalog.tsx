7f602618cce12a424a0db47b728a382e8ecd85f9
Stefan Kirilov <stefank5248@gmail.com>
2024-10-11 11:20:33 +0300

    new

---
+++ client/buyCycle-olx/src/components/Catalog/Catalog.tsx
@@ -0,0 +1,13 @@
+import styles from './catalog.module.css';
+
+import { Link } from 'react-router-dom';
+
+export default function Catalog() {
+
+
+    return(
+        <>
+        <h1>Hello</h1>
+        </>
+    )
+}
