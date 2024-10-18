d8ee0d8a81309c2a47d57301b7ef197b0918948a
Stefan Kirilov <stefank5248@gmail.com>
2024-10-18 17:09:40 +0300

    new commit catalog

--- client/buyCycle-olx/src/components/Catalog/Catalog.tsx
+++ client/buyCycle-olx/src/components/Catalog/Catalog.tsx
@@ -1,11 +1,15 @@
 import styles from './catalog.module.css';
 
-import { useEffect, useState } from 'react';
+import heart from '../../assets/red_heart.svg'
+import fillHeart from '../../assets/red_fill_heart.svg'
+
+import { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import * as apiService from '../../services/apiService';
 import { getImageURL } from '../../utils/image-util';
 import moment from 'moment';
+import AuthContext from '../../contexts/authContext';
 
 type Cycle = {
     name: string,
@@ -27,15 +31,40 @@ type Cycle = {
 
 
 export default function Catalog() {
+    const { userId, isAuthenticated }: any = useContext(AuthContext);
+
     const [cycles, setSCycles] = useState<Cycle[]>([]);
+    const [isLiked, setLike] = useState(false);
 
     useEffect(() => {
         apiService.getAllCycles()
             .then(data => setSCycles(data))
             .catch(error => console.log(error));
 
-            return () => console.log('Unmount component!');
-    }, [])
+        return () => console.log('Unmount component!');
+    }, [isLiked])
+
+
+    const like = (id: any) => {
+
+        console.log('1');
+
+        apiService.like(id)
+            .then(() => setLike(!isLiked))
+            .catch();
+
+    }
+
+    const unlike = (id: any) => {
+
+        console.log('2');
+        
+        apiService.unlike(id)
+            .then(() => setLike(!isLiked))
+            .catch();
+
+    }
+
     return (
         <div className={styles.top}>
             <h1 className={styles['category-name']}>Всички обяви</h1>
@@ -43,17 +72,25 @@ export default function Catalog() {
                 {cycles?.map(data => (
                     <div key={data._id} className={styles['container-second']}>
                         <Link to={`/${data._id}`}>
-                        <div className={styles["container-img"]}>
-                            {/* <img className={styles.image} src={data?.image[0]} /> */}
-                            <img className={styles.image} src={getImageURL(data?.image[0])} />
-                        </div>
+                            <div className={styles["container-img"]}>
+                                {/* <img className={styles.image} src={data?.image[0]} /> */}
+                                <img className={styles.image} src={getImageURL(data?.image[0])} />
+                            </div>
+                        </Link>
                         <div className={styles["container-items"]}>
-                            <p className={styles.name}>{data.name}</p>
+                            <div>
+                                <p className={styles.name}>{data.name}</p>
+                                {
+                                    (data.likes.find(id => id == userId)) ?
+                                        <img onClick={() => unlike(data._id)} className={styles.bin} src={fillHeart} alt="" />
+                                        :
+                                        <img onClick={() => like(data._id)} className={styles.bin} src={heart} alt="" />
+                                }
+                            </div>
                             <p className={styles.price}>{data.price} лв.</p>
                             <p className={styles.place}>{data.place}</p>
                             <p className={styles.data}>{moment(data.date).format('D MMM, YYYY')}</p>
                         </div>
-                        </Link>
                     </div>
                 ))}
             </div >
