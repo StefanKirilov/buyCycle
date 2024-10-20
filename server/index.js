748cf5cc1cc089f8a82712e059469fe51956434a
Stefan Kirilov <stefank5248@gmail.com>
2024-10-20 18:18:42 +0300

    new commit index server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const multer = require('multer');
 const routes = require('./routes');
 const cookieParser = require('cookie-parser');
 
+const userService = require('./services/userService');
+
 const app = express();
+
+// const fs = require('fs');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './Images')
+    },
+    filename: (req, file, cb) => {  
+        cb(null, Date.now() + path.extname(file.originalname));  
+    }
+})
+
+const upload = multer({storage: storage})
+
 app.use(cors({
     origin: 'http://localhost:5173',
     allowedHeaders: ['Content-Type', 'Authorization'],
@@ -16,6 +33,53 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(routes);
+
+// upload the file
+app.use("/Images", express.static("Images"));
+
+app.post('/upload',upload.single('file') , async (req, res) => {
+    const user = JSON.parse(req.cookies["auth"]);
+    const userId = user?._id;
+
+    if (!req.file) {
+        console.log("Select image file to upload!");
+        return
+    }
+    if (req.file.mimetype !== 'image/jpeg') {
+        console.log("Please select JPG image to upload!"); 
+        return
+    };
+
+    // res.json(filename);
+
+    const data = req.file.filename;
+
+    // const filename = req.file.filename;
+
+    try {      
+        let user = await userService.uploadImage(data, userId)
+        console.log(user);
+        
+        res.json(user);
+    } catch (error) {
+        console.log(error.message);
+    }
+
+});
+
+// const deleteFile = (filename) => {
+//     const filePath = path.join(__dirname, 'Images', filename);
+
+//     fs.unlink(filePath, (err) => {
+//         if (err) {
+//             console.error("Error while deleting file:", err);
+//         } else {
+//             console.log(`File ${filename} deleted successfully`);
+//         }
+//     });
+// };
+
+// module.exports = { deleteFile }
 
 mongoose.connect('mongodb://localhost:27017/buyCycle')
 .then(() => console.log('DB connected'));
