8bcff37460138400078ee7c75d7fc96e31007d16
Stefan Kirilov <stefank5248@gmail.com>
2024-10-27 21:13:50 +0200

    commit cycle controller

--- server/controllers/cycleController.js
+++ server/controllers/cycleController.js
@@ -1,27 +1,35 @@
 const router = require('express').Router();
 const cycleServices = require('../services/cycleServices');
 
-router.get('/',async (req, res) => {
+router.get('/', async (req, res) => {
     const cycles = await cycleServices.getAll();
     res.json(cycles);
 });
 
-router.post('/',async (req, res) => {
+router.post('/', async (req, res) => {
     const cycleData = req.body;
-    // const user = req.cookies["auth"]?._id;
-    const cycles = await cycleServices.create({...cycleData, owner: req.cookies.auth._id});
+    const user = JSON.parse(req.cookies["auth"]);
+    const userId = user?._id;
+    const date = new Date().toISOString();
+
+    const cycles = await cycleServices.create({
+        name: cycleData.form.name, description: cycleData.form.description, price: cycleData.form.price,
+        condition: cycleData.form.condition, place: cycleData.form.place, phone: cycleData.form.phone, type: cycleData.form.type, image: cycleData.images,
+        email: cycleData.email,date: date, owner: userId
+    });
 
     res.json(cycles);
+
 });
 
-router.get('/:cycleId',async (req, res) => {
+router.get('/:cycleId', async (req, res) => {
     const cycleId = req.params.cycleId;
     const cycle = await cycleServices.getOne(cycleId);
 
     res.json(cycle);
 });
 
-router.put('/:cycleId',async (req, res) => {
+router.put('/:cycleId', async (req, res) => {
     const cycleId = req.params.cycleId;
     const cycleData = req.body;
     const cycle = await cycleServices.update(cycleId, cycleData);
@@ -29,11 +37,11 @@ router.put('/:cycleId',async (req, res) => {
     res.json(cycle);
 });
 
-router.delete('/:cycleId',async (req, res) => {
+router.delete('/:cycleId', async (req, res) => {
     const cycleId = req.params.cycleId;
     await cycleServices.delete(cycleId);
 
-    res.status(204).json({ok: true});
+    res.status(204).json({ ok: true });
 });
 
 router.get('/:cycleId/like', async (req, res) => {
@@ -45,7 +53,7 @@ router.get('/:cycleId/like', async (req, res) => {
 
     try {
         await cycleServices.like(cycleId, userId);
-        res.status(204).json({ok: true});
+        res.status(204).json({ ok: true });
     } catch (error) {
         console.log(error.message);
     }
@@ -59,7 +67,7 @@ router.get('/:cycleId/unlike', async (req, res) => {
 
     try {
         await cycleServices.unlike(cycleId, userId);
-        res.status(204).json({ok: true});
+        res.status(204).json({ ok: true });
     } catch (error) {
         console.log(error.message);
     }
@@ -72,8 +80,8 @@ router.post('/:cycleId/comments', async (req, res) => {
     const date = new Date().toISOString();
 
     try {
-        await cycleServices.addComment(cycleId, { user , comment, username, date});
-        res.status(204).json({ok: true});
+        await cycleServices.addComment(cycleId, { user, comment, username, date });
+        res.status(204).json({ ok: true });
     } catch (error) {
         console.log(error.message);
     }
@@ -85,7 +93,7 @@ router.post('/:cycleId/deleteComment', async (req, res) => {
     const { elementId } = req.body;
     try {
         await cycleServices.deleteComment(cycleId, elementId);
-        res.status(204).json({ok: true});
+        res.status(204).json({ ok: true });
     } catch (error) {
         console.log(error.message);
     }
