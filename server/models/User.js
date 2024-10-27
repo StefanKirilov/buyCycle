2ee2e4ac4e6caddad44e7fbc2a7c5ad3a0e09809
Stefan Kirilov <stefank5248@gmail.com>
2024-10-27 21:15:01 +0200

    commit user model for mongoDB

--- server/models/User.js
+++ server/models/User.js
@@ -14,6 +14,9 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Password is required'],
     },
+    image: [{
+        type: String,
+    }],
 });
 
 userSchema.pre ('save',async function(next) {
