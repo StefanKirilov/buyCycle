6d4c15e1a9279664284e78d0ad2db2aea8cdb768
Stefan Kirilov <stefank5248@gmail.com>
2024-10-28 13:53:21 +0200

    commit app.js

--- client/buyCycle-olx/src/App.tsx
+++ client/buyCycle-olx/src/App.tsx
@@ -2,20 +2,42 @@
 import './App.css'
 
 import { Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './contexts/authContext';
 
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
 import Create from './components/Create/Create'
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import Logout from './components/Logout/Logout';
+import Cycle from './components/DetailCycle/DetailCycle';
+
+import AuthGuard from './components/guards/AuthGuard';
+import NotAuthGuard from './components/guards/NotAuthGuard';
 
 function App() {
 
   return (
     <>
-      <Header />
-      <Routes >
-        <Route path="/" element={<Home />} />
-        <Route path="/create" element={<Create />} />
-      </Routes>
+      <AuthProvider>
+        <Header />
+        <Routes >
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/:id" element={<Cycle />}></Route>
+
+          <Route element={<AuthGuard />}>
+            {/* <Route path="/favorites" element={<Favorites />} />
+              <Route path="/profile" element={<Profile />} /> */}
+            <Route path="/logout" element={<Logout />} />
+          </Route>
+
+          <Route element={<NotAuthGuard />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </>
   )
 }
