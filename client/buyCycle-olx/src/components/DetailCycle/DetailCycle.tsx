6cb7f68a3fa777ac6ca26b969a451ea714f37f2a
Stefan Kirilov <stefank5248@gmail.com>
2024-10-30 16:25:38 +0200

    commit details

--- client/buyCycle-olx/src/components/DetailCycle/DetailCycle.tsx
+++ client/buyCycle-olx/src/components/DetailCycle/DetailCycle.tsx
@@ -1,9 +1,12 @@
+import profileIcon from "../../assets/profile-icon.png";
+
 import styles from "./detail.module.css";
 import heart from "../../assets/red_heart.svg";
 import fillHeart from "../../assets/red_fill_heart.svg";
 
 import { useContext, useEffect, useState } from "react";
 import * as apiService from "../../services/apiService";
+import * as userService from "../../services/userService";
 import { useParams } from "react-router-dom";
 import AuthContext from "../../contexts/authContext";
 import { getImageURL } from "../../utils/image-util";
@@ -27,11 +30,20 @@ type Cycle = {
   _id: string;
 };
 
+type User = {
+  email: "";
+  image: [];
+  username: "";
+  _id: "";
+};
+
 export default function Cycle() {
   const [cycle, setCycle] = useState<Cycle>();
+  const [owner, setOwner] = useState<User>();
   const [isLike, setLike] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isFullscreen, setFullscreen] = useState(false);
+  const [showPhone, setShowPhone] = useState(false);
 
   const { userId, isAuthenticated }: any = useContext(AuthContext);
   const { id } = useParams();
@@ -42,6 +54,15 @@ export default function Cycle() {
       .then((data) => setCycle(data))
       .catch((error) => console.log(error));
   }, [isLike]);
+
+  useEffect(() => {
+    if (cycle) {
+      userService
+        .getOwner(cycle?.owner)
+        .then((data) => setOwner(data))
+        .catch((error) => console.log(error));
+    }
+  }, [cycle]);
 
   const goToNextSlide = () => {
     if (!cycle) return;
@@ -84,6 +105,10 @@ export default function Cycle() {
       document.exitFullscreen();
       setFullscreen(false);
     }
+  };
+
+  const toggleShowPhone = () => {
+    setShowPhone((state) => !state);
   };
 
   return (
@@ -153,12 +178,11 @@ export default function Cycle() {
         <div className={styles.contact}>
           <div className={styles.user}>
             <h3>КОНТАКТ С ПРОДАВАЧА</h3>
-            <div>
-              <img src="" alt="" />
+            <div className={styles.owner}>
+              <img src={profileIcon} alt="profileIcon" />
               <div>
-                <p>sadsafsd</p>
-                <p>fdssfdsfds</p>
-                <p>sdffsdfdsfds</p>
+                <p>Username: {owner?.username}</p>
+                <p>Email: {owner?.email}</p>
               </div>
             </div>
           </div>
@@ -166,33 +190,46 @@ export default function Cycle() {
           <div className={styles.contactButtons}>
             <button className={styles.msg}>Съобщение</button>
             <div className={styles["phone-container"]}>
-              <p className={styles.phone}>xxx-xxx-xxx</p>
-              <button className={styles.view}>Покажи</button>
+              <p className={styles.phone}>
+                {showPhone ? `0${cycle?.phone}` : "xxx-xxx-xxx"}
+              </p>
+              <button onClick={toggleShowPhone} className={styles.view}>
+                Покажи
+              </button>
             </div>
           </div>
         </div>
       </div>
 
       <div className={styles["right-container"]}>
-        <div className={styles["first-container-items"]}>
-          <div className={styles["date"]}>
-            <span>{moment(cycle?.date).format("D MMM, YYYY")}</span>
-            <div className={styles.heart}>
-              {cycle?.likes.find((id) => id == userId) ? (
-                <img
-                  onClick={() => unlike(cycle?._id)}
-                  src={fillHeart}
-                  alt=""
-                />
-              ) : (
-                <img onClick={() => like(cycle?._id)} src={heart} alt="" />
-              )}
+        <div>
+          <div className={styles["first-container-items"]}>
+            <div className={styles["date"]}>
+              <span>{moment(cycle?.date).format("D MMM, YYYY")}</span>
+              <div className={styles.heart}>
+                {cycle?.likes.find((id) => id == userId) ? (
+                  <img
+                    onClick={() => unlike(cycle?._id)}
+                    src={fillHeart}
+                    alt=""
+                  />
+                ) : (
+                  <img onClick={() => like(cycle?._id)} src={heart} alt="" />
+                )}
+              </div>
+            </div>
+            <h1 className={styles.name}> {cycle?.name} </h1>
+            <p className={styles.price}>{cycle?.price} лв.</p>
+            <div className={styles.btnMsg}>
+              <button>Съобщение</button>
             </div>
           </div>
-          <h1 className={styles.name}> {cycle?.name} </h1>
-          <p className={styles.price}>{cycle?.price} лв.</p>
-          <div className={styles.btnMsg}>
-            <button>Съобщение</button>
+        </div>
+
+        <div>
+          <div className={styles["first-container-items"]}>
+            <h2>Локация</h2>
+            <div className={styles.location}></div>
           </div>
         </div>
       </div>
