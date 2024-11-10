52a592174f3e0224551976b50563c51e72acfff0
Stefan Kirilov <stefank5248@gmail.com>
2024-11-10 19:17:37 +0200

    commit user services

--- client/buyCycle-olx/src/services/userService.ts
+++ client/buyCycle-olx/src/services/userService.ts
@@ -1,152 +1,155 @@
-const baseUrl = 'http://localhost:5000';
-
+const baseUrl = "http://localhost:5000";
 
 export const login = async (data: any) => {
-    try {
-        const response = await fetch(
-            `${baseUrl}/users/login`,
-            {
-                method: 'POST',
-                headers: {'Content-Type':'application/json'},
-                body: JSON.stringify(data)
-            }
-        )
-    
-        const result = await response.json();
-        return result;
-    } catch (error) {
-        
-    }
-}
+  try {
+    const response = await fetch(`${baseUrl}/users/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+
+    const result = await response.json();
+    return result;
+  } catch (error) {}
+};
 
 export const register = async (data: any) => {
-    try {
-        const response = await fetch(
-            `${baseUrl}/users/register`,
-            {
-                method: 'POST',
-                headers: {'Content-Type':'application/json'},
-                body: JSON.stringify(data)
-            }
-        )     
+  try {
+    const response = await fetch(`${baseUrl}/users/register`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
 
-        if (response.status == 409) {
-            throw new Error ('The user already registered!')            
-        }
-
-        const result = await response.json();
-        return result;
-    } catch (error) {
-
+    if (response.status == 409) {
+      throw new Error("The user already registered!");
     }
-}
+
+    const result = await response.json();
+    return result;
+  } catch (error) {}
+};
 
 export const logout = async () => {
-    try {
-        const response = await fetch(
-            `${baseUrl}/users/logout`,
-            {
-                method: 'GET',
-            }
-        )
-    
-        if (response.status === 204) {
-            return {};
-        };
-    
-        const result = await response.json();
-    
-        if (!response.ok) {
-            throw result;
-        }
-    
-        return result;
-    } catch (error) {
-        console.log(error);      
-    }
-}
+  try {
+    const response = await fetch(`${baseUrl}/users/logout`, {
+      method: "GET",
+    });
 
-export const deleteFile = async (fileName: any) => { 
-    try {
-    const response = await fetch(`${baseUrl}/users/${fileName}`,
-        {
-            method: "DELETE",
-            credentials: "include",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        }
+    if (response.status === 204) {
+      return {};
+    }
+
+    const result = await response.json();
+
+    if (!response.ok) {
+      throw result;
+    }
+
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deleteFile = async (fileName: any) => {
+  try {
+    const response = await fetch(`${baseUrl}/users/${fileName}`, {
+      method: "DELETE",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.status === 204) {
+      return {};
+    }
+
+    const result = await response.json();
+
+    if (!response.ok) {
+      throw result;
+    }
+
+    return result;
+    // return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getOwner = async (id: any) => {
+  try {
+    const response = await fetch(
+      `${baseUrl}/users/owner`,
+
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ id }),
+      }
     );
-    
-        if (response.status === 204) {
-            return {};
-        };
 
-        const result = await response.json();
-    
-        if (!response.ok) {
-            throw result;
-        }
-        
-        return result;
-        // return response;
-    } catch (error) {
-        console.log(error);
+    if (response.status == 409) {
+      throw new Error("The user already registered!");
     }
-}
+
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const getProfile = async () => {
-    try {
-        const response = await fetch(
-            `${baseUrl}/users/profile`,
-            {
-                credentials: "include",
-                headers: {
-                    'Content-Type': 'application/json',
-                }
-            });
-    
-        if (response.status === 204) {
-            return {};
-        };
-    
-        const result = await response.json();
-    
-        if (!response.ok) {
-            throw result;
-        }
-    
-        return result;
-    } catch (error) {
-        console.log(error);
-    }
-  }
+  try {
+    const response = await fetch(`${baseUrl}/users/profile`, {
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-  export const editProfile = async (data: any) => {
-    try {
-        const response = await fetch(
-            `${baseUrl}/users/profile`,
-            {
-                method: "PUT",
-                credentials: "include",
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(data)
-            });
-    
-        if (response.status === 204) {
-            return {};
-        };
-    
-        const result = await response.json();
-    
-        if (!response.ok) {
-            throw result;
-        }
-        
-        return result;
-    } catch (error) {
-        console.log(error);
+    if (response.status === 204) {
+      return {};
     }
+
+    const result = await response.json();
+
+    if (!response.ok) {
+      throw result;
+    }
+
+    return result;
+  } catch (error) {
+    console.log(error);
   }
+};
+
+export const editProfile = async (data: any) => {
+  try {
+    const response = await fetch(`${baseUrl}/users/profile`, {
+      method: "PUT",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (response.status === 204) {
+      return {};
+    }
+
+    const result = await response.json();
+
+    if (!response.ok) {
+      throw result;
+    }
+
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
