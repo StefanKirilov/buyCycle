32a514aa29b827dc8199b1891bf3a7b16c22ebb2
Stefan Kirilov <stefank5248@gmail.com>
2024-11-10 19:16:54 +0200

    commit context

--- client/buyCycle-olx/src/context/authContext.tsx
+++ client/buyCycle-olx/src/context/authContext.tsx
@@ -1,76 +1,67 @@
-import { createContext, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { bake_cookie, read_cookie, delete_cookie } from 'sfcookies';
+import { bake_cookie, read_cookie, delete_cookie } from "sfcookies";
 
-import * as userService from '../services/userService';
+import * as userService from "../services/userService";
 
-const AuthContext = createContext('');
+const AuthContext = createContext("");
 
-let cookie = read_cookie('auth');
+let cookie = read_cookie("auth");
 // console.log(cookie);
 
-export const AuthProvider = ({
-    children,
-}: any) => {
-    const navigate = useNavigate();
-    const [auth, setAuth] = useState<any>(read_cookie('auth'));
+export const AuthProvider = ({ children }: any) => {
+  const navigate = useNavigate();
+  const [auth, setAuth] = useState<any>(read_cookie("auth"));
 
-    const loginSubmitHandler = async (data: any) => {
-        const result = await userService.login(data);
-        setAuth(result);
+  const loginSubmitHandler = async (data: any) => {
+    const result = await userService.login(data);
+    setAuth(result);
 
-        console.log(result);
-        
-        bake_cookie('auth', result);
+    bake_cookie("auth", result);
 
-        navigate("/");
-    }
+    navigate("/");
+  };
 
-    const registerSubmitHandler = async (data: any) => {
-        
-        const result = await userService.register(data);
-        setAuth(result);
+  const registerSubmitHandler = async (data: any) => {
+    const result = await userService.register(data);
+    setAuth(result);
 
-        bake_cookie('auth', result);
+    bake_cookie("auth", result);
 
-        navigate("/");
-    }
+    navigate("/");
+  };
 
-    const logoutHandler = async () => {
-        setAuth({});
+  const logoutHandler = async () => {
+    setAuth({});
 
-        delete_cookie('auth');
+    delete_cookie("auth");
 
-        // 
-        // bake_cookie('shop', []);
+    //
+    // bake_cookie('shop', []);
 
-        navigate("/");
-    }
+    navigate("/");
+  };
 
-    const editProfileSubmitHandler = async (data: any) => {
+  const editProfileSubmitHandler = async (data: any) => {
+    const result = await userService.editProfile(data);
 
-        const result = await userService.editProfile(data);
+    setAuth(result);
 
-        setAuth(result);
+    bake_cookie("auth", result);
+  };
 
-        bake_cookie('auth', result);
-    }
-
-    const values: any = {
-        loginSubmitHandler,
-        registerSubmitHandler,
-        logoutHandler,
-        editProfileSubmitHandler,
-        userId: auth._id,
-        email: auth.email,
-        isAuthenticated: !!auth.accessToken,
-    }
-    return (
-        <AuthContext.Provider value={values}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
+  const values: any = {
+    loginSubmitHandler,
+    registerSubmitHandler,
+    logoutHandler,
+    editProfileSubmitHandler,
+    userId: auth._id,
+    email: auth.email,
+    username: auth.username,
+    isAuthenticated: !!auth.accessToken,
+  };
+  return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
+};
 
 export default AuthContext;
