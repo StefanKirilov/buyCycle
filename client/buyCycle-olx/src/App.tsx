89df88ce8a6ab0c43a2fd172a4ce41353e4cdd2f
Stefan Kirilov <stefank5248@gmail.com>
2024-11-10 19:18:15 +0200

    commit app

--- client/buyCycle-olx/src/App.tsx
+++ client/buyCycle-olx/src/App.tsx
@@ -1,34 +1,37 @@
+import "./App.css";
 
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./contexts/authContext";
 
-import { Route, Routes } from 'react-router-dom';
-import { AuthProvider } from './contexts/authContext';
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Create from "./components/Create/Create";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Logout from "./components/Logout/Logout";
+import Cycle from "./components/DetailCycle/DetailCycle";
+import Footer from "./components/Footer/Footer";
+import Favorites from "./components/Favorites/Favorites";
+import MyItems from "./components/MyItems/MyItems";
+import Profile from "./components/Profile/Profile";
 
-import Header from './components/Header/Header'
-import Home from './components/Home/Home'
-import Create from './components/Create/Create'
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
-import Logout from './components/Logout/Logout';
-import Cycle from './components/DetailCycle/DetailCycle';
-
-import AuthGuard from './components/guards/AuthGuard';
-import NotAuthGuard from './components/guards/NotAuthGuard';
+import AuthGuard from "./components/guards/AuthGuard";
+import NotAuthGuard from "./components/guards/NotAuthGuard";
 
 function App() {
-
   return (
     <>
       <AuthProvider>
         <Header />
-        <Routes >
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create" element={<Create />} />
           <Route path="/:id" element={<Cycle />}></Route>
 
           <Route element={<AuthGuard />}>
-            {/* <Route path="/favorites" element={<Favorites />} />
-              <Route path="/profile" element={<Profile />} /> */}
+            <Route path="/likes" element={<Favorites />} />
+            <Route path="/poster" element={<MyItems />} />
+            <Route path="/profile" element={<Profile />} />
             <Route path="/logout" element={<Logout />} />
           </Route>
 
@@ -38,8 +41,9 @@ function App() {
           </Route>
         </Routes>
       </AuthProvider>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
